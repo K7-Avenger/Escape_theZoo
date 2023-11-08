10732fa34c64bc1bd450ca07a2ceb08c48e36ca4
K7-Avenger <48218837+K7-Avenger@users.noreply.github.com>
2023-11-08 16:10:01 -0600

    Create lister.ps1

---
+++ lister.ps1
@@ -0,0 +1,46 @@
+# Define the directory you want to list items from
+$directoryPath = "C:\Users\Cybr442\Desktop\Malx-main\theZoo\theZoo-master\malware\Binaries"
+$zipPassword = "infected"
+
+# Load the System.IO.Compression.FileSystem assembly
+Add-Type -TypeDefinition @"
+    using System.IO.Compression;
+"@
+
+# Initialize an empty list to store the items
+$itemsList = @()
+
+# Check if the specified directory exists
+if (Test-Path -Path $directoryPath -PathType Container) {
+    # Get all items (files and directories) in the specified directory
+    $items = Get-ChildItem -Path $directoryPath
+
+    # Add each item to the list
+    foreach ($item in $items) {
+        #$itemsList += $item.FullName
+        $dirs_to_open = $item.FullName
+        $jtems = Get-ChildItem -Path $dirs_to_open
+        
+        foreach($jtem in $jtems){
+            $exten = [IO.Path]::GetExtension($jtem)
+            if ($exten -eq ".zip" ){
+                Write-Host $dirs_to_open
+                Write-Host $jtem.FullName
+                try {
+                    [System.IO.Compression.ZipFile]::ExtractToDirectory(
+                        $jtem.FullName,
+                        $dirs_to_open,
+                        $zipPassword
+                    )
+                    Write-Host "Successfully extracted the ZIP file."
+                } catch {
+                    Write-Host "Failed to extract the ZIP file. Error: $($_.Exception.Message)"
+                }
+            }
+            
+        }
+    }
+}
+else {
+    Write-Host "The specified directory does not exist."
+}
